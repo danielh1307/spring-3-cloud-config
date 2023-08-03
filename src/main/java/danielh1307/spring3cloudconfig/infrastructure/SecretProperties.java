92a264ae00aec9dc4efd550d0484b51d55ad27ec
Daniel <hamm.daniel@gmail.com>
2023-08-03 20:01:20 +0200

    Update of secret works without lombok and fabric8.

--- src/main/java/danielh1307/spring3cloudconfig/infrastructure/SecretProperties.java
+++ src/main/java/danielh1307/spring3cloudconfig/infrastructure/SecretProperties.java
@@ -1,10 +1,8 @@
 package danielh1307.spring3cloudconfig.infrastructure;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Data
 @Configuration
 @ConfigurationProperties(prefix = "aad")
 public class SecretProperties {
